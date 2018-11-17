c1a0e59126dc7a63d7636354d1c19a50af8f7f86
aqiao292 <angelaqiao621@gmail.com>
2018-11-16 19:24:52 -0800

    Scanning the robot class

--- Parser/src/Parsing1.java
+++ Parser/src/Parsing1.java
@@ -1,4 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Parsing1 {
 
+	public static void main(String[] args) throws FileNotFoundException {
+		parseRobot("driveTrain","");
+	}
+	public static void parseRobot(String variable, String function) throws FileNotFoundException {
+		Scanner scan = new Scanner(new File("/Users/Angela/git/CHS_SIM/Simulator/src/org/usfirst/frc/team2473/robot/Robot.java"));
+		String type = "n";
+		boolean found = false;
+		while(scan.hasNextLine() && !found) {
+			String line = scan.nextLine();
+			//System.out.println(line);
+			if(line.contains(" "+variable)) {
+				StringTokenizer st = new StringTokenizer(line,"; ");
+				String previous = "";
+				while(st.hasMoreTokens()) {
+					String current = st.nextToken();
+					//System.out.println(current);
+					if(current.equals(variable)) {
+						type = previous;
+						found = true;
+						break;
+					}
+					previous = current;
+				}
+			}
+		}
+		System.out.println(type);
+	}
+	
 }

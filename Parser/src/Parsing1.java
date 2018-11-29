a24131108358f484db85ea9de6535a889bf47e1f
Jerry Liu <jbliu52@gmail.com>
2018-11-28 20:43:09 -0800

    completed egg function

--- Parser/src/Parsing1.java
+++ Parser/src/Parsing1.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -34,7 +35,45 @@ public class Parsing1 {
 		}
 		egg(type,function);
 	}
+	
 	public static void egg(String cl, String func) {
+		File file = new File(robotPath);
+		Scanner input;
+		String code = "";
+		try {
+			input = new Scanner(file);
+			String line = "";
+			boolean reading = false;
+			int paren = 0;
+			while(input.hasNextLine()) {
+				line = input.nextLine();
+				if(line.contains(func) && (line.contains("{") || line.contains("}"))) {
+					reading = true;
+				}
+				if(reading) {
+					while(Character.isWhitespace(line.charAt(0))) {
+						line = line.substring(1);
+						code += "\t";
+					}
+					code += line + "\n";
+					for(char c : line.toCharArray()) {
+						if(c == '{') {
+							paren++;
+						}else if(c == '}') {
+							paren--;
+						}
+					}
+					if(paren == 0) {
+						break;
+					}
+				}
+			}
+			input.close();
+		}catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
 		
+		System.out.println(code);
+		return code;
 	}
 } 
